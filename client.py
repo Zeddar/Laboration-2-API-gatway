ae9929248665a3c5407d2f1e3f338ef9c4652678
Jack Andersson <jack.andersson@student.kyh.se>
2022-12-06 12:45:04 +0100

    Refresh looping test

--- client.py
+++ client.py
@@ -2,10 +2,16 @@ import requests
 
 
 def main():
-    response = requests.get("http://localhost:9080/temp/get/")
-    values = response.json()
-
-    print(values)
+    menu_loop = True
+    while menu_loop:
+        response = requests.get("http://localhost:9080/temp/get/")
+        values = response.json()
+        print(values)
+        val = input("Refresh? Y/N ")
+        if val.lower() == 'n':
+            menu_loop = False
+        else:
+            menu_loop = True
 
 
 if __name__ == "__main__":
